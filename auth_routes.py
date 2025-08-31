7b5cc597942b588b23929f8964620b9bdf347609
Uan <uanesfrancisco@gmail.com>
2025-08-31 11:53:38 -0300

    fix: padroniza as entradas de parametros do endpoint /create

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,9 @@
+from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException
 from main import bcrypt_context
 from models import User
 from dependencies import get_session
+from schemas import UserSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -13,15 +15,15 @@ async def home():
     return {"message": "You are on the login route", "authenticated": False}
 
 @auth_router.post("/create")
-async def create_user(email:str, password:str, name:str, session = Depends(get_session)):
+async def create_user(user_schema: UserSchema, session: Session = Depends(get_session)):
 
-    user = session.query(User).filter(User.email == email).first()
+    user = session.query(User).filter(User.email == user_schema.email).first()
     if user:
         raise HTTPException(status_code=400, detail= "Alredy exist a user with this email") 
     else:
-        crypt_password = bcrypt_context.hash(password)
-        new_user = User(name= name, email= email, password= crypt_password)
+        crypt_password = bcrypt_context.hash(user_schema.password)
+        new_user = User(name= user_schema.name, email= user_schema.email, password= crypt_password, is_admin= user_schema.is_admin, is_active= user_schema.is_active)
         session.add(new_user)
         session.commit()
-        return {"message": f"User add with sucess! {email}"}
+        return {"message": f"User add with sucess! {user_schema.email}"}
         
