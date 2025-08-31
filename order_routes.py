c06fcdabe1741ba85643d4399d2c7b8ad8a2e00b
Uan <uanesfrancisco@gmail.com>
2025-08-31 12:17:55 -0300

    feat: adiciona o endpoint de criar pedidos

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import get_session
+from models import Order
+from schemas import OrderSchema
 
 order_router = APIRouter(prefix="/order", tags=["order"])
 
@@ -8,3 +12,10 @@ async def get_orders():
     This route is used to get all orders, but only if the user is authenticated
     """
     return {"message": "You are on the order route"}
+
+@order_router.post("/create")
+async def create_order(order_schema: OrderSchema, session: Session = Depends(get_session)):
+    new_order = Order(user= order_schema.user_id)
+    session.add(new_order)
+    session.commit()
+    return {"message": f"Order add with sucess. Order id: {new_order.id}"}
