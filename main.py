aa3cd8656bc6c113cb437822369a5114955005e6
Uan <uanesfrancisco@gmail.com>
2025-08-31 11:01:02 -0300

    feat: adiciona criptografia para a senha dos usuarios

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from warnings import deprecated
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated= "auto")
 
 from auth_routes import auth_router
 from order_routes import order_router
